05e08c10390958b6a08cfd1832d1867fc4b3c1f0
Victor Nicollet <victor@nicollet.net>
2013-11-21 12:01:00 +0100

    Implement token generation

--- server/tokenLib/i.ml
+++ server/tokenLib/i.ml
@@ -1,7 +1,23 @@
 (* © 2013 RunOrg *)
 
+open Std
+
 include Id.Phantom
 
 module Assert = struct
   let server_admin id = id
 end
+
+(* We cannot use the standard generation function here, because its output is 
+   predictable. Instead, we generate an HMAC of a standard identifier, using the
+   configured token key. This HMAC is converted to base62 and truncated. 
+   
+   For 1 000 000 simultaneous sessions, there is a 3e-6 probability of collision
+   (because the 11-character base62 identifier is generated from a random 
+   64-bit value, with a single base62 character dropped). *)
+let gen () = 
+  let unsafe_token_id = gen () in 
+  let hmac = Sha1.hmac Configuration.token_key (to_string unsafe_token_id) in
+  let b62 = String.base62_encode hmac in 
+  of_string (String.sub b62 0 Id.length)
+
--- server/tokenLib/store.ml
+++ server/tokenLib/store.ml
@@ -1,5 +1,7 @@
 (* © 2013 RunOrg *)
 
+open Std
+
 module Owner = type module [ `ServerAdmin ]
 type owner = Owner.t
 
@@ -12,16 +14,41 @@ type owner = Owner.t
 
 let dbname = Cqrs.Names.independent "tokens" 1 
 
-let () = Cqrs.Sql.on_first_connection (Cqrs.Sql.command begin 
-  "CREATE TABLE IF NOT EXISTS \"" ^ dbname ^ "\" ( "
-  ^ "\"token\" CHAR(40) NOT NULL, "
-  ^ "\"payload\" BYTEA NOT NULL, "
-  ^ "\"created\" TIMESTAMP NOT NULL DEFAULT('now'), "
-  ^ "PRIMARY KEY(\"token\"))"
-end [])
+let () = 
+  Cqrs.Sql.on_first_connection (Cqrs.Sql.command begin 
+    "CREATE TABLE IF NOT EXISTS \"" ^ dbname ^ "\" ( "
+    ^ "\"token\" CHAR(40) NOT NULL, "
+    ^ "\"payload\" BYTEA NOT NULL, "
+    ^ "\"created\" TIMESTAMP NOT NULL DEFAULT('now'), "
+    ^ "PRIMARY KEY(\"token\"))"
+  end []) ;
 
+  (* 2 days is longer than the actual session life : this is a cleanup function, 
+     not an invalidation function. Invalidation happens when loading data. *)
+  Cqrs.Sql.on_first_connection (Cqrs.Sql.command begin 
+    "DELETE FROM \"" ^ dbname ^ "\" WHERE \"created\" < 'now' - interval '2 days'"
+  end [])
 
+(* Creating new sessions 
+   ===================== *)
 
-let create owner = assert false
+let create owner =
+  let  id = I.gen () in
+  let! () = Cqrs.Sql.command ("INSERT INTO \"" ^ dbname ^ "\" (\"token\", \"payload\") VALUES ($1, $2)")
+    [ `String (I.to_string id) ; `Binary (Pack.to_string Owner.pack owner) ] in
+  return id 
+
+(* Loading old sessions
+   ==================== *)
+
+let session_life = "interval '1 hour'"
+
+let load id = 
+  let! result = Cqrs.Sql.query begin 
+    "SELECT \"payload\" FROM \"" ^ dbname ^ "\" "
+    ^ "WHERE \"token\" = $1 AND \"created\" < 'now' - " ^ session_life
+  end [ `String (Id.to_string id) ] in
+  return (Cqrs.Result.unpack result Owner.unpack)
+
 let is_server_admin id = assert false
 
