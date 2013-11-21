21dfd0e053e5ce257deb34bedf0da2e53706563d
Victor Nicollet <victor@nicollet.net>
2013-11-21 18:49:27 +0100

    Unescape BYTEA results

--- server/cqrsLib/result.ml
+++ server/cqrsLib/result.ml
@@ -7,5 +7,8 @@ let string r =
     if Array.length r.(0) = 0 then None else
       Some r.(0).(0)
 
+let bytes r = 
+  Option.map Postgresql.unescape_bytea (string r)
+
 let unpack r unpacker = 
-  Option.map (Pack.of_string unpacker) (string r)
+  Option.map (Pack.of_string unpacker) (bytes r)
