87c1ca5ff54c0f5915006b9aded17735e31476a3
Victor Nicollet <victor@nicollet.net>
2013-11-21 09:57:30 +0100

    Expect token to list all admins

--- server/apiLib/serverAdmin.ml
+++ server/apiLib/serverAdmin.ml
@@ -2,17 +2,20 @@
 
 open Std
 
+(* List of all registered administrators 
+   ===================================== *)
+
 include Endpoint.Get(struct
 
   module Admin = type module < email : string >
   let wrap email = Admin.make ~email
 
-  module Arg = type module unit
+  module Arg = type module < token : Token.I.t >
   module Out = type module < admins : Admin.t list >
 
   let path = "admin/all" 
 
-  let response req () = 
+  let response req a = 
     let list = Configuration.admins in 
     return (`OK (Out.make ~admins:(List.map wrap list)))
 
