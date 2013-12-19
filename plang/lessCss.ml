1b0d828b6b9e82136f482465000e5b233726faed
Victor Nicollet <victor@nicollet.net>
2013-12-19 13:46:16 +0100

    LESS CSS invocation

---
+++ plang/lessCss.ml
@@ -0,0 +1,11 @@
+(* © 2013 RunOrg *)
+
+let compile input output = 
+  let result = Sys.command (Printf.sprintf "lessc --yui-compress %s > %s"
+			      (Filename.quote input) (Filename.quote output)) in
+  if result <> 0 then exit (-1) ;
+  let chan = open_in output in
+  let length = in_channel_length chan in 
+  close_in chan ; 
+  Printf.printf "%s: %d bytes\n%!" output length 
+ 
