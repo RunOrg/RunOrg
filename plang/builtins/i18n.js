4eec17e79cd98f9ba4bf7ae73aae27cd12a8d29b
Victor Nicollet <victor@nicollet.net>
2013-12-19 12:50:03 +0100

    Fix i18n filling

--- plang/builtins/i18n.js
+++ plang/builtins/i18n.js
@@ -2,10 +2,10 @@
 
 function i18n(obj) {
     for (var key in obj) {
-	var segs = key.split("."), root = i18n;
-	for (var i = 0; i < segs.length - 1; ++i) 
-	    root = (root[i] = root[i] || {});
-	root[segs.length-1] = obj[key];
+	var segs = key.split("."), root = i18n, i;
+	for (i = 0; i < segs.length - 1; ++i) 
+	    root = (root[segs[i]] = root[segs[i]] || {});
+	root[segs[i]] = obj[key];
     }
 }
 
