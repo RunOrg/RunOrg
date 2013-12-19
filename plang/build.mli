3a20ee0e453b65133d70ffca53f205777af7a393
Victor Nicollet <victor@nicollet.net>
2013-12-19 09:55:09 +0100

    Build interface

---
+++ plang/build.mli
@@ -0,0 +1,13 @@
+(* © 2013 RunOrg *)
+
+(** Builds the output CSS and JavaScript from an exploration. *)
+
+(** The type of a result: the CSS and JS source code. *)
+type t = {
+  css : string ;
+  js  : string ; 
+}
+
+(** Build the output. Optionally provide a 'builtins' string, otherwise will use 
+    [./builtins] as the source for built-in javascript and CSS. *)
+val build : ?builtins:string -> Explore.t -> t
