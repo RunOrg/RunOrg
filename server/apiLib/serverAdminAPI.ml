b4f595fa657ca48a154362c6fc8e89b9bfa8c1f5
Victor Nicollet <victor@nicollet.net>
2013-12-19 12:35:12 +0100

    Expose english admin i18n

--- server/apiLib/serverAdminAPI.ml
+++ server/apiLib/serverAdminAPI.ml
@@ -11,6 +11,7 @@ let bad_auth  = `Forbidden "Could not log in as server administrator"
 let () = 
   Endpoint.static "admin" "text/html" "static/admin/index.html" ;
   Endpoint.static "admin/script.js" "text/javascript" "_assets/admin/all.js" ;
+  Endpoint.static "admin/en.js" "text/javascript" "_assets/admin/en.js" ;
   Endpoint.static "admin/style.css" "text/css" "_assets/admin/all.css" ;
   Endpoint.static "admin/logo.png" "image/png" "static/admin/logo-runorg-50x50.png"
 
