f8e043987ec6b1e0118c488982d30fb3b2cc921a
Victor Nicollet <victor@nicollet.net>
2013-12-19 09:43:03 +0100

    Doc fix

--- plang/tplGen.mli
+++ plang/tplGen.mli
@@ -3,6 +3,7 @@
 (** Compiling templates to JavaScript code. *)
 
 (** Compiles all the provided templates to JavaScript. Each template is provided
-    along with its path. *)
+    along with its path. Intended to be inserted as variable {{ TEMPLATES }} into
+    the builtin files. *)
 val compile : (string list * TplAst.file) list -> string
 
