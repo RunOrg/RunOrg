963dcdc180398cdd0bc312bdfbe17011ee7086fb
Victor Nicollet <victor@nicollet.net>
2013-12-20 14:59:47 +0100

    Implement RunOrg database module

---
+++ server/db.ml
@@ -0,0 +1,24 @@
+(* © 2013 RunOrg *)
+
+open Std
+
+(* Event stream 
+   ============ *)
+
+module Event = type module 
+    [ `DatabaseCreated of string
+    ]
+
+module Stream = Cqrs.Stream(struct
+  include Event
+  let name = "db"
+end)
+
+(* Commands 
+   ======== *)
+
+let create _ label = 
+  let  id = Id.gen () in
+  let  event = `DatabaseCreated label in
+  let! clock = Run.edit_context (fun ctx -> ctx # with_db id) (Stream.append [event]) in
+  return (id, clock) 
