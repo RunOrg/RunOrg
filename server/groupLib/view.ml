94fa0569a5280a527d5fff027744b0cf831c91da
Victor Nicollet <victor@nicollet.net>
2014-01-20 17:02:19 +0100

    Implement group count

--- server/groupLib/view.ml
+++ server/groupLib/view.ml
@@ -4,48 +4,29 @@ open Std
 
 let projection = Cqrs.Projection.make "group" (fun () -> new O.ctx)
 
-(* Group information 
-   ================= *)
+(* Group existence 
+   =============== *)
 
-module Info = type module <
-  label : string option 
->
-
-let info = 
+let exists = 
   
-  let infoV, info = Cqrs.MapView.make projection "info" 0 
-    (module I : Fmt.FMT with type t = I.t)
-    (module Info : Fmt.FMT with type t = Info.t) in
+  let existsV, exists = Cqrs.SetView.make projection "exists" 0 
+    (module I : Fmt.FMT with type t = I.t) in
 
   (* Create the admin group the first time it is mentioned. *)
   let ensure_admin id = 
     if I.to_string id = "admin" 
-    then Cqrs.MapView.update info id (function None -> `Put (Info.make ~label:None) | _ -> `Keep)
+    then Cqrs.SetView.add exists [id] 
     else return ()
   in
 
-  let () = Store.track infoV begin function 
-
-    | `Created ev -> 
-
-      Cqrs.MapView.update info (ev # id) 
-	(function 
-	| None   -> `Put (Info.make ~label:(ev # label))
-	| Some _ -> `Keep) 
-
-    | `Deleted ev ->
-
-      Cqrs.MapView.update info (ev # id) (fun _ -> `Delete)
-
-
-    | `Added ev 
-    | `Removed ev -> 
-
-      List.M.iter ensure_admin (ev # groups)
-
+  let () = Store.track existsV begin function 
+    | `Created ev -> Cqrs.SetView.add exists [ev # id] 
+    | `Deleted ev -> Cqrs.SetView.remove exists [ev # id]
+    | `Added   ev 
+    | `Removed ev -> List.M.iter ensure_admin (ev # groups)
   end in 
 
-  info
+  exists
 
 (* Contacts in group 
    ================= *)
@@ -69,8 +50,54 @@ let contacts =
 
     | `Added ev ->
 
-      Cqrs.ManyToManyView.add contacts (ev # groups) (ev # contacts) 
+      let! groups = List.M.filter (Cqrs.SetView.exists exists) (ev # groups) in
+      if groups = [] then return () else 
+	Cqrs.ManyToManyView.add contacts (ev # groups) (ev # contacts) 
 
   end in 
 
   contacts
+
+(* Group information 
+   ================= *)
+
+module Info = type module <
+  label : string option ;
+  count : int ; 
+>
+
+let info = 
+  
+  let infoV, info = Cqrs.MapView.make projection "info" 0 
+    (module I : Fmt.FMT with type t = I.t)
+    (module Info : Fmt.FMT with type t = Info.t) in
+
+  (* Create the admin group the first time it is mentioned. *)
+  let count gid = 
+    let! count = Cqrs.ManyToManyView.count contacts gid in 
+    Cqrs.MapView.update info gid (function
+      | None -> if I.to_string gid = "admin" then `Put (Info.make ~label:None ~count:0) else `Keep
+      | Some g -> if g # count = count then `Keep else `Put (Info.make ~label:(g#label) ~count))
+  in
+
+  let () = Store.track infoV begin function 
+
+    | `Created ev -> 
+
+      Cqrs.MapView.update info (ev # id) 
+	(function 
+	| None   -> `Put (Info.make ~label:(ev # label) ~count:0)
+	| Some _ -> `Keep) 
+
+    | `Deleted ev ->
+
+      Cqrs.MapView.update info (ev # id) (fun _ -> `Delete)
+
+    | `Added ev 
+    | `Removed ev -> 
+
+      List.M.iter count (ev # groups)
+
+  end in 
+
+  info
