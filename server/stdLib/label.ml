ac50de6414e14658bbebbbfaef692adbe7982f90
Victor Nicollet <victor@nicollet.net>
2014-01-24 13:46:45 +0100

    Implement label validation

--- server/stdLib/label.ml
+++ server/stdLib/label.ml
@@ -1,1 +1,108 @@
 (* © 2014 RunOrg *)
+
+(* String cleanup function
+   ======================= *)
+
+exception BadUTF8 of int
+
+let of_string_detailed str = 
+
+  let n = String.length str in
+
+  let nextUTF8 i =     
+    if i = n then raise (BadUTF8 i) else
+      let c = Char.code str.[i] in
+      if c < 0x70 then 
+	i + 1, c 
+      else if c < 0xA0 then 
+	raise (BadUTF8 i) 
+      else if c < 0xE0 then
+	if i + 1 = n then raise (BadUTF8 i) else
+	  let c2 = Char.code str.[i+1] in
+	  if c2 / 64 <> 2 then raise (BadUTF8 i) else
+	    i + 2, (c mod 32) * 64 + (c2 mod 64) 
+      else if c < 0xF0 then
+	if i + 2 = n then raise (BadUTF8 i) else
+	  let c2 = Char.code str.[i+1] in
+	  let c3 = Char.code str.[i+2] in 
+	  if c2 / 64 <> 2 then raise (BadUTF8 i) else
+	    if c3 / 64 <> 2 then raise (BadUTF8 i) else
+	      i + 3, ((c mod 16) * 64 + (c2 mod 64)) * 64 + (c3 mod 64) 
+      else
+	if i + 3 = n then raise (BadUTF8 i) else
+	  let c2 = Char.code str.[i+1] in
+	  let c3 = Char.code str.[i+2] in 
+	  let c4 = Char.code str.[i+3] in 
+	  if c2 / 64 <> 2 then raise (BadUTF8 i) else
+	    if c3 / 64 <> 2 then raise (BadUTF8 i) else
+	      if c4 / 64 <> 2 then raise (BadUTF8 i) else
+		i + 4, (((c mod 8) * 64  + (c2 mod 64)) * 64 + (c3 mod 64)) * 64 + (c4 mod 64) 
+  in
+
+  let buffer = Buffer.create n in 
+  
+  let rec cleanup start i sp nbsp c = 
+    if i = n then       
+      if sp then 
+	max 0 (c - 1) (* Trim all space at the end. *)
+      else
+	( Buffer.add_substring buffer str start (i - start) ; c )
+    else
+      let i2, utf8 = nextUTF8 i in 
+      let space = match utf8 with 
+	| 0x09 | 0x0A | 0x0D | 0xA0 | 0x2028 | 0x2029 | 0xFFFE | 0xFEFF -> true 
+	| _ -> false in 
+      let nbsp2 = utf8 = 0xA0 in
+      if sp <> space then 
+	if sp then begin 
+	  (* Found a non-space character after a whitespace sequence *)
+	  if start > 0 (* Trim all space at the front. *) then 
+	    if nbsp then Buffer.add_string buffer "\xC2\xA0" else Buffer.add_char buffer ' ' ;
+	  cleanup i i2 false false (c + 1)
+	end else begin 
+	  (* Found a whitespace character after a non-space sequence *)
+	  Buffer.add_substring buffer str start (i - start) ;
+	  cleanup i i2 true nbsp2 (c + 1)
+	end
+      else
+	cleanup start i2 sp (nbsp || nbsp2) (if sp then c else (c + 1))
+  in
+
+  try let n = cleanup 0 0 true false 0 in
+      if n = 0 then `Empty else
+	if n >= 80 then `TooLong n else
+	  `Ok (Buffer.contents buffer) 
+  with BadUTF8 i -> `UTF8 i
+
+(* Parser format (overload JSON loading)
+   ===================================== *)
+
+module S = type module (string)
+
+include Fmt.Make(struct
+  type t = string
+  let pack = S.pack
+  let unpack = S.unpack
+  let json_of_t = S.to_json 
+
+  let t_of_json json = 
+    let s = S.of_json json in    
+    match of_string_detailed s with 
+    | `Ok s -> s
+    | `Empty -> raise (Json.Error "Empty label")
+    | `UTF8 n -> raise (Json.Error "Invalid UTF-8 at byte %d") 
+    | `TooLong n -> raise (Json.Error (Printf.sprintf "Label is %d code points long, only 80 allowed." n))
+
+end)
+
+(* Conversion functions 
+   ==================== *)
+
+let of_string str = 
+  match of_string_detailed str with 
+  | `Ok s -> Some s 
+  | `Empty
+  | `UTF8 _  
+  | `TooLong _ -> None
+
+let to_string lbl = lbl
--- server/stdLib/label.mli
+++ server/stdLib/label.mli
@@ -1,2 +1,29 @@
 (* © 2014 RunOrg *)
 
+(** A label is a short UTF-8 string, no longer than 80 code points, and not empty.
+
+    The label is always trimmed and cleaned up. Initial and final whitespace
+    (understood as javascript's regex character \s) are dropped, all whitespace
+    characters are turned into either normal or non-breaking spaces (depending on their
+    nature) and multiple consecutive whitespace characters are turned into only one 
+    (a normal space, or a non-breaking space if only non-breaking spaces are found). 
+
+    Length tests occur after trimming and cleanup.
+
+    Note that in JavaScript, [string.length] returns the size in an UTF16 encoding, 
+    which is always greater than the number of code points. It is therefore acceptable
+    to use [label.trim().replace('\s+',' ').length < 80] as a test on the maximum length 
+    of the string.
+
+    Since RunOrg uses UTF-8 encoding for labels, a label may be up to 320 bytes long.
+*)
+
+include Fmt.FMT
+
+(** Attempt to create a label from a string. Return [None] if the string does not
+    satisfy the requirements. *)
+val of_string : string -> t option 
+
+(** Returns the string representation of a label. *)
+val to_string : t -> string 
+
