a75aaba142c741c2b7eba528c87d4a92a19318cd
Victor Nicollet <victor@nicollet.net>
2014-01-24 15:59:15 +0100

    Factor out UTF8 reading

---
+++ server/stdLib/common.ml
@@ -0,0 +1,38 @@
+(* © 2014 RunOrg *)
+
+(* String cleanup function
+   ======================= *)
+
+exception BadUTF8 of int
+
+let nextUTF8 str = 
+  let n = String.length str in 
+  fun i -> 
+    if i = n then raise (BadUTF8 i) else
+      let c = Char.code str.[i] in
+      if c < 0x70 then 
+	i + 1, c 
+      else if c < 0xA0 then 
+	raise (BadUTF8 i) 
+      else if c < 0xE0 then
+	if i + 1 = n then raise (BadUTF8 i) else
+	  let c2 = Char.code str.[i+1] in
+	  if c2 / 64 <> 2 then raise (BadUTF8 i) else
+	    i + 2, (c mod 32) * 64 + (c2 mod 64) 
+      else if c < 0xF0 then
+	if i + 2 = n then raise (BadUTF8 i) else
+	  let c2 = Char.code str.[i+1] in
+	  let c3 = Char.code str.[i+2] in 
+	  if c2 / 64 <> 2 then raise (BadUTF8 i) else
+	    if c3 / 64 <> 2 then raise (BadUTF8 i) else
+	      i + 3, ((c mod 16) * 64 + (c2 mod 64)) * 64 + (c3 mod 64) 
+      else
+	if i + 3 = n then raise (BadUTF8 i) else
+	  let c2 = Char.code str.[i+1] in
+	  let c3 = Char.code str.[i+2] in 
+	  let c4 = Char.code str.[i+3] in 
+	  if c2 / 64 <> 2 then raise (BadUTF8 i) else
+	    if c3 / 64 <> 2 then raise (BadUTF8 i) else
+	      if c4 / 64 <> 2 then raise (BadUTF8 i) else
+		i + 4, (((c mod 8) * 64  + (c2 mod 64)) * 64 + (c3 mod 64)) * 64 + (c4 mod 64) 
+  
--- server/stdLib/label.ml
+++ server/stdLib/label.ml
@@ -1,44 +1,14 @@
 (* © 2014 RunOrg *)
 
+open Common 
+
 (* String cleanup function
    ======================= *)
-
-exception BadUTF8 of int
 
 let of_string_detailed str = 
 
   let n = String.length str in
-
-  let nextUTF8 i =     
-    if i = n then raise (BadUTF8 i) else
-      let c = Char.code str.[i] in
-      if c < 0x70 then 
-	i + 1, c 
-      else if c < 0xA0 then 
-	raise (BadUTF8 i) 
-      else if c < 0xE0 then
-	if i + 1 = n then raise (BadUTF8 i) else
-	  let c2 = Char.code str.[i+1] in
-	  if c2 / 64 <> 2 then raise (BadUTF8 i) else
-	    i + 2, (c mod 32) * 64 + (c2 mod 64) 
-      else if c < 0xF0 then
-	if i + 2 = n then raise (BadUTF8 i) else
-	  let c2 = Char.code str.[i+1] in
-	  let c3 = Char.code str.[i+2] in 
-	  if c2 / 64 <> 2 then raise (BadUTF8 i) else
-	    if c3 / 64 <> 2 then raise (BadUTF8 i) else
-	      i + 3, ((c mod 16) * 64 + (c2 mod 64)) * 64 + (c3 mod 64) 
-      else
-	if i + 3 = n then raise (BadUTF8 i) else
-	  let c2 = Char.code str.[i+1] in
-	  let c3 = Char.code str.[i+2] in 
-	  let c4 = Char.code str.[i+3] in 
-	  if c2 / 64 <> 2 then raise (BadUTF8 i) else
-	    if c3 / 64 <> 2 then raise (BadUTF8 i) else
-	      if c4 / 64 <> 2 then raise (BadUTF8 i) else
-		i + 4, (((c mod 8) * 64  + (c2 mod 64)) * 64 + (c3 mod 64)) * 64 + (c4 mod 64) 
-  in
-
+  let nextUTF8 = nextUTF8 str in     
   let buffer = Buffer.create n in 
   
   let rec cleanup start i sp nbsp c = 
