29036a46b63e6903a5005aa2aa92c036116aee7c
Victor Nicollet <victor@nicollet.net>
2014-01-27 15:49:59 +0100

    Naked test for grabbing a contact

---
+++ test/contact/get.js
@@ -0,0 +1,8 @@
+// GET /db/{db}/contacts/{id}
+// Contacts / Fetch basic information for contact {id}
+
+TEST("Returns 404 when database does not exist.", function() {
+});
+
+TEST("Returns 404 when contact does not exist in database.", function() {
+});
