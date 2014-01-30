cc69b9d7762be235ac56a684e5e440dea71a3c82
Victor Nicollet <victor@nicollet.net>
2014-01-30 14:35:41 +0100

    Parse version and status

--- testUI/parse.js
+++ testUI/parse.js
@@ -1,7 +1,7 @@
 /* Parsing test sets */
 function parseTestFixture(script) {
 
-    var parsed = { tests: [] };
+    var parsed = { tests: [], status: null, version: null };
 
     // The test functions themselves
     var run = eval("(function(TEST){\n" + script + "\n})")(function(name,run){
@@ -13,10 +13,18 @@ function parseTestFixture(script) {
     var text  = [];
     for (var i = 0; i < lines.length; ++i) 
 	if (/^\s*\/\//.exec(lines[i])) 
-	    text.push(lines[i].replace(/^\s*\/\/\s?/,''));
-	
+	    text.push(lines[i].replace(/^\s*\/\/\s?/,''));	    
+
     parsed.query = text.shift();
-    parsed.description = text.shift().replace(/^.*\/\s*/,'');
+    parsed.description = text.shift().replace(/^.*\/\s*/,'');   
+
+    while (text[0].trim() == '') text.shift();
+
+    if (/@/.exec(text[0])) {
+	var current = text.shift().split('@');
+	parsed.status = current[0].trim();
+	parsed.version = current[1].trim();
+    }
 
     parsed.body = markdown.toHTML(text.join('\n').trim());
 
