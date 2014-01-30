4aff29ee2027e2b61a3feba76f9ed8b14205d58a
Victor Nicollet <victor@nicollet.net>
2014-01-30 10:39:09 +0100

    Individual node page start

--- testUI/ctrl.js
+++ testUI/ctrl.js
@@ -1,48 +1,60 @@
 /* Simple controller. */
 
-/* Renders the sidebar. */
-function sidebar(R) {
-    Test.tree(function(tree) {
-	function clean(tree) {
-	    var out = { sub: [], tests: [], count: 0, ok: true, ran: true };
-	    for (var k in tree) {
-		var node = tree[k];
-		if (k == '__') continue;
-		if (node['__'] == 'cat') {
-		    var s = clean(node);
-		    s.name = k;
-		    out.sub.push(s);
-		    out.count += s.count;
-		    out.ok = out.ok && s.ok;
-		} else {                
-		    out.tests.push({ 
-			name: k, 
-			ok:   !node.failed,
-			ran:  node.ran,
-			verb: node.file.verb,
-			path: node.file.path,
-			file: "/docs/" + node.file.file,
-			count: node.file.tests
-		    });
-		    out.count += node.file.tests;
-		    out.ok = out.ok && !node.failed;
-		    out.ran = out.ran && node.ran;
+/* Extend the layout to automatically render the sidebar. */
+(function() {
+
+    var layout = R.layout;
+
+    function sidebar(R) {
+	Test.tree(function(tree) {
+	    function clean(tree) {
+		var out = { sub: [], tests: [], count: 0, ok: true, ran: true };
+		for (var k in tree) {
+		    var node = tree[k];
+		    if (k == '__') continue;
+		    if (node['__'] == 'cat') {
+			var s = clean(node);
+			s.name = k;
+			out.sub.push(s);
+			out.count += s.count;
+			out.ok = out.ok && s.ok;
+		    } else {                
+			out.tests.push({ 
+			    name: k, 
+			    ok:   !node.failed,
+			    ran:  node.ran,
+			    verb: node.file.verb,
+			    path: node.file.path,
+			    file: "/docs/" + node.file.file,
+			    count: node.file.tests
+			});
+			out.count += node.file.tests;
+			out.ok = out.ok && !node.failed;
+			out.ran = out.ran && node.ran;
+		    }
 		}
+		out.ran = out.ran && out.count > 0;
+		return out;
 	    }
-	    out.ran = out.ran && out.count > 0;
-	    return out;
-	}
+	    
+	    R.sidebar(clean(tree));
+	    R.show();
+	});
+    }
 
-	R.sidebar(clean(tree));
-	R.show();
-    });
-}
+    R.layout = function(data) {
+	layout.call(this,$.extend({ sidebar : sidebar }, data || {}));
+    }
 
-Route.add(/^\/docs/, function(R) {
+})();
 
-    R.layout({
-	sidebar: sidebar
-    });
-
+/* Root page */
+Route.add(/^\/docs\/?$/, function(R) {
+    R.layout();
     R.show();
+});
+
+/* Individual node pages */
+Route.add(/^\/docs\/(.+)$/, function(R) {
+    console.log(arguments);
 });
