4cbf91c016cb54420bfc4cd5c1d698fe5e07dc8f
Victor Nicollet <victor@nicollet.net>
2014-01-31 17:54:46 +0100

    Fix doc: custom ids

--- test/types/id.js
+++ test/types/id.js
@@ -6,6 +6,10 @@
 // (lexicographically) after any identifiers generated earlier, though this does not
 // always imply the corresponding objects have been created in that order.
 // 
+// Other processes, notably [custom identifiers](/docs/#/types/custom-id.js), support
+// the creation of identifiers between 1 and 10 characters long. A typical example
+// is the `admin` group. 
+//
 // An unique identifier satisfies the regular expression `[a-zA-Z0-9]{1,11}`. 
 //
 // ### Example value
