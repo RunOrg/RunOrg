c7b83ef04c5e4ac895f72b6e3ee4def6800c4380
Victor Nicollet <victor@nicollet.net>
2014-01-31 15:43:21 +0100

    Assert equality testing

--- testUI/assert.js
+++ testUI/assert.js
@@ -1,7 +1,39 @@
 var Assert = (function(){
 
+    function equal(a,b) {
+	if (typeof a != typeof b) return false;
+	if (typeof a == "object" && Array.isArray(a)) {
+	    if (a.length != b.length) return false;
+	    for (var i = 0; i < a.length; ++i)
+		if (!equal(a[i],b[i])) return false;
+	    return true;
+	}
+	if (typeof a == "object") {
+	    for (var k in b) 
+		if (b.hasOwnProperty(k) && !a.hasOwnProperty(k)) return false;
+	    for (k in a) {
+		if (a.hasOwnProperty(k) && !b.hasOwnProperty(k)) return false;
+		if (!equal(a[k],b[k])) return false;
+	    }
+	    return true;
+	}
+	return a === b;
+    }
+
     return {
-	fail: function() { Test.fail("Assert.fail()"); }
+
+	fail: function() { Test.fail("Assert.fail()"); },
+
+	areEqual: function(a,b) {
+
+	    return Async.lift([a,b]).map(function(p){
+		var a = p[0], b = p[1]; 
+		if (equal(a,b)) return Async.lift(true);
+		else Test.fail("Not equal: "+JSON.stringify(a)+" and "+JSON.stringify(b));
+	    });
+
+	}
+
     };
 
 })();
