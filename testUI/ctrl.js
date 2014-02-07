f2279d7ed1413912209625c50c2b285ca9b1a5d9
Victor Nicollet <victor@nicollet.net>
2014-02-07 09:54:45 +0100

    Display node sub-elements in body

--- testUI/ctrl.js
+++ testUI/ctrl.js
@@ -10,9 +10,24 @@ Route.add(/^\/docs(\/(#\/?)?)?$/, function(R) {
 Route.add(/^\/docs\/#\/(.+)$/, function(R,path) {
 
     function body(R) {
-	Test.get(path,function(contents) {
-	    R.body(contents);
-	    R.show();
+	Test.all(function(all) {
+	    Test.get(path,function(contents) {
+
+		// Extract all sub-elements and display them in a 
+		// pretty table format
+		var meta = all[path], inner = [];
+		if ('cat' in meta) {
+		    for (var k in meta.cat) {
+			if (k == '__' || k == '_f') continue;
+			var sub = meta.cat[k];
+			if (sub.__ == 'cat') continue;
+			inner.push(sub.fixture);
+		    }
+		}
+
+		R.body($.extend({inner:inner},contents));
+		R.show();
+	    });
 	});
     }
 
