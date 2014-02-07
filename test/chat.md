b0a90f29a8b1afeff6ef25467ca7ca2843ecdd52
Victor Nicollet <victor@nicollet.net>
2014-02-07 14:23:19 +0100

    Chatroom documentation

--- test/chat.md
+++ test/chat.md
@@ -9,3 +9,14 @@ or individual [contacts](/docs/#/contact.md).
 Chatroom messages are called **items**, and are not necessarily pieces
 of text (arbitrary content is allowed, with the client being
 responsible for rendering any custom formats).
+
+Some chatrooms implement **private messaging** (abbreviated as `pm`): 
+ - they have a fixed list of participants (two contacts);
+ - they have no label or name;
+ - they are only accessible to those participants (no reading or 
+   writing by anyone else);
+ - for any pair of contacts, there exists at most one `pm` chatrooms,
+   which is _the_ private messaging history for those two contacts.
+
+Non-`pm` chatrooms are free in terms of participants, read/write
+access, and so on. 
---
+++ test/chat/get.js
@@ -0,0 +1,80 @@
+// GET /db/{db}/chat/{id}
+// Chatrooms / Read chatroom properties
+//
+// `200 OK`, 
+// [Read-only](/docs/#/concept/read-only.md).
+//
+// Reads chatroom information (except the list of messages). 
+// Data is returned using a [client join pattern](/docs/#/concept/client-join.md): 
+//
+// ### Response format
+//     { "contacts": [ <shortcontact>, ... ],
+//       "info": <chatinfo> }
+//  - `contacts` is a list of [short profiles](/docs/#/contact/short-contact.js) for 
+//    all participants in the chatroom.
+//  - `info` is a [chatroom information object](/docs/#/chat/chat-info.js).
+
+TEST("The response has valid return code and content type.", function(next) {
+});
+
+// # Examples
+// 
+// ### Example request
+//     GET /db/0SNQc00211H/chat/0SNQe0132FW
+// 
+// ### Example response
+//     200 OK
+//     Content-Type: application/json 
+//     
+//     { "contacts": [
+//       { "id" : "0SNQe00311H",
+//         "name" : "test@runorg.com",
+//         "gender" : null,
+//         "pic" : "https://www.gravatar.com/avatar/1ed54d253636f5b33eff32c2d5573f70" },
+//       { "id" : "0SNQg00511H",
+//         "name" : "vnicollet@runorg.com",
+//         "gender" : null,
+//         "pic" : "https://www.gravatar.com/avatar/5a31b00f649489a9a24d3dc3e8b28060"} ],
+//       "info": { 
+//         "id": 0SNQe0132FW,
+//         "contacts": ["0SNQe00311H","0SNQg00511H"],
+//         "groups": [],
+//         "count": 118 }}
+
+TEST("Returns correct list of contacts.", function(next) {
+});
+
+TEST("Returns correct list of groups.", function(next) {
+});
+
+TEST("Returns correct message count.", function(next) {
+});
+
+// 
+// # Errors
+// 
+// ## Returns `404 Not Found`
+// - ... if database `{db}` does not exist
+
+TEST("Returns 404 when database does not exist.", function(next) {
+});
+
+// - ... if chatroom `{id}` does not exist in database `{db}`
+
+TEST("Returns 404 when chatroom does not exist.", function(next) {
+});
+
+// ## Returns `401 Unauthorized` 
+// - ... if the provided token does not grant access to the chatroom,
+//   or no token was provided
+
+TEST("Returns 401 when token is not valid.", function(next) {
+});
+ 
+// # Access restrictions
+//
+// Currently, anyone can read the basic details of a chatroom with a token for the corresponding database. 
+// This is subject to change in future versions.
+       
+
+
