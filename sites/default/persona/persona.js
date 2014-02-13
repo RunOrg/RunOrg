b6d8b801dd5bf301c0ac7cf49b68cbb3486888c6
Victor Nicollet <victor@nicollet.net>
2014-02-13 09:23:34 +0100

    React to HTTP 401

---
+++ sites/default/persona/persona.js
@@ -0,0 +1,17 @@
+/* Authenticating with Persona. */
+
+api.onLoginRequired = function() {
+    var R = Route.replace(), self = this;
+    R["persona/page"]({ button: function(R) {
+	R.$[0].onclick = function() {
+	    navigator.id.get(function(assertion){
+		self.AUTH('auth/persona', {assertion:assertion}, function(r){
+		    Route.dispatch();
+		});		
+	    },{
+		siteName: i18n.title		
+	    });
+	};
+    }});
+    R.show();
+};
