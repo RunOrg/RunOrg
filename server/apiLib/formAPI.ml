853454abe93d69b3c7e00839f55e56866206c5d3
Victor Nicollet <victor@nicollet.net>
2014-04-03 17:40:06 +0200

    Implement PUT forms/{id}

--- server/apiLib/formAPI.ml
+++ server/apiLib/formAPI.ml
@@ -34,3 +34,43 @@ module Create = Endpoint.Post(struct
 	       | Some id -> return (`Accepted (Out.make ~id ~at))
 
 end)
+
+(* Updating a form
+   =============== *)
+
+module Update = Endpoint.Put(struct
+
+  module Arg = type module < id : Form.I.t >
+  module Put = type module <
+    ?label : String.Label.t option ;
+    ?custom : Json.t = Json.Null ;
+    ?owner : Form.Owner.t option ;
+    ?fields : Field.t list option ;
+    ?audience : Form.Audience.t option ;
+  >
+
+  module Out = type module <
+    at : Cqrs.Clock.t 
+  >
+
+  let path = "forms/{id}"
+
+  let response req arg put = 
+    
+    (* Special treatment to separate "null" fields from missing fields *)
+    let label = match req # body with 
+      | Some (`JSON (Json.Object obj)) when List.exists (fun (k,_) -> k = "label") obj -> Some (put # label)
+      | _ -> None
+    and custom = match req # body with 
+      | Some (`JSON (Json.Object obj)) when List.exists (fun (k,_) -> k = "custom") obj -> Some (put # custom)
+      | _ -> None
+    and owner = put # owner and fields = put # fields and audience = put # audience in 
+
+    let! result = Form.update ?label ?custom ?owner ?fields ?audience (arg # id) in
+
+    match result with 
+    | `OK at -> return (`Accepted (Out.make ~at))
+    | `NoSuchForm id -> return (`NotFound (!! "Form '%s' does not exist." (Form.I.to_string id)))
+    | `FormFilled id ->  return (`BadRequest (!! "Form '%s' is already filled." (Form.I.to_string id)))
+
+end)
