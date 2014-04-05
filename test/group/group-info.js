307723c3787fc352326e4e001f0649d98fa0d302
Victor Nicollet <victor@nicollet.net>
2014-04-05 13:46:15 +0200

    Group data tests

--- test/group/group-info.js
+++ test/group/group-info.js
@@ -16,20 +16,55 @@
 // - `id` is the [unique 11-character identifier](/docs/#/types/id.js) for this 
 //   contact
 
-TEST("The groups's identifier is returned.", function() {
-    Assert.fail();
+TEST("The groups's identifier is returned.", function(next) {
+
+    var example = {};
+
+    var db = Query.mkdb();
+    var auth = Query.auth(db);
+
+    var id = Test.query("POST",["db/",db,"/groups/create"],example,auth).result("id");
+    var id2 = Test.query("GET",["db/",db,"/groups/",id,"/info"],auth).result("id");
+
+    Assert.areEqual(id,id2).then(next);
+
 });
 
 // - `label` is a [human-readable name](/docs/#/types/label.js). Not all groups
 //   have one, because it is not a mandatory property when creating a group.
-TEST("The groups's label is returned if available.", function() {
-    Assert.fail();
+TEST("The groups's label is returned if available.", function(next) {
+
+    var example = { "label" : "Associates" };
+
+    var db = Query.mkdb();
+    var auth = Query.auth(db);
+
+    var id = Test.query("POST",["db/",db,"/groups/create"],example,auth).result("id");
+    var label = Test.query("GET",["db/",db,"/groups/",id,"/info"],auth).result("label");
+
+    Assert.areEqual(example.label,label).then(next);
+
 });
 
 // - `count` is the number of group members (contacts currently in the group).
 
-TEST("The group's member count is returned.", function() {
-    Assert.fail();
+TEST("The group's member count is returned.", function(next) {
+
+    var example = { "label" : "Associates" };
+
+    var db = Query.mkdb();
+    var auth = Query.auth(db);
+
+    var id = Test.query("POST",["db/",db,"/groups/create"],example,auth).result("id");
+    
+    Test.query("POST",["db/",db,"/groups/",id,"/add"],[auth.id],auth).result().then(function(){
+
+	var count = Test.query("GET",["db/",db,"/groups/",id,"/info"],auth).result("count");
+
+	Assert.areEqual(1, count).then(next);
+
+    });
+
 });
 
 // ### Example value
