198504b4e632363b461c609b641afa6abc3b9d7d
Victor Nicollet <victor@nicollet.net>
2014-04-06 10:05:38 +0200

    Docs for GET /forms

---
+++ test/form/list.js
@@ -0,0 +1,155 @@
+// GET /db/{db}/forms
+// Forms / List all forms
+// 
+// Alpha @ 0.1.41
+//
+// `200 OK`, 
+// [Read-only](/docs/#/concept/read-only.md),
+// [Paginated](/docs/#/concept/paginated.md).
+//
+// Returns all the forms that can be seen by contact `{as}`, in arbitrary order.
+// Supports `limit` and `offset` pagination. 
+//
+// ### Response format
+//     { "list": [ <short-form>, ... ] }
+// - `list` is a list of forms on the requested page, in 
+//   [short format](/docs/#/format/short.js)
+
+TEST("The response has valid return code and content type.", function(next) {
+
+    var db = Query.mkdb();
+    var response = Test.query("GET",["db/",db,"/forms"]).response();
+
+    response.map(function(r) {
+	Assert.areEqual(200, r.status).then();
+	Assert.isTrue(r.responseJSON, "Response type is JSON").then();
+    }).then(next);
+
+});
+
+// # Examples
+// 
+// ### Example request
+//     GET /db/0SNQc00211H/forms?as=0Sd7003511H&limit=3&offset=213
+// 
+// ### Example response
+//     200 OK
+//     Content-Type: application/json 
+//     
+//     { "list" : [ 
+//       { "id" : "0SNQe00311H",
+//         "owner" : "contact",
+//         "label" : "Why did you join our group ?", 
+//         "fields" : 2,
+//         "access" : ["fill"] },
+//       { "id" : "0SNQg00511H",
+//         "label" : null,
+//         "owner" : "contact", 
+//         "fields" : 1,
+//         "pic" : ["admin","fill"] }
+
+TEST("Returns data for all forms.", function(next) {
+
+    var exampleA = {
+	"owner": "contact",	
+	"audience": { "fill": "anyone" },
+	"label": "Join form",
+	"custom": [1,2,3],
+	"fields": [ {
+	    "id": "1",
+	    "label": "Why did you join our group ?",
+	    "kind": "text",
+	    "required": true
+	}, {
+	    "id": "2",
+	    "label": "How often do you log in ?",
+	    "kind": "single",
+            "choices" : [
+		"Every day",
+		"Once a week",
+		"Once a month",
+		"Less than once a month" ],
+	    "required": false
+	} ]
+    };
+
+    var exampleB = {
+	"owner": "contact",	
+	"audience": { "admin": "anyone" },
+	"label": "Personal information",
+	"fields": [ {
+	    "id": "1",
+	    "label": "First pet's name",
+	    "kind": "text",
+	    "required": false
+	} ]
+    };
+
+    var exampleC = {
+	"owner": "contact",	
+	"audience": {},
+	"label": "Private information",
+	"fields": [ {
+	    "id": "1",
+	    "label": "Your favourite color",
+	    "kind": "text",
+	    "required": false
+	} ]
+    };
+
+
+    var db = Query.mkdb();
+    var auth = Query.auth(db);
+
+    Test.query("POST",["db/",db,"/forms/create"],exampleA,auth).result("id").map(function(idA) {
+	Test.query("POST",["db/",db,"/forms/create"],exampleB,auth).result("id").map(function(idB) {
+	    Test.query("POST",["db/",db,"/forms/create"],exampleC,auth).result("id").map(function(idC) {
+
+		var expected = {
+		    "list" : [ {
+			"id": idA,
+			"owner": "contact",
+			"access": ["fill"],
+			"fields": 2,
+			"label": exampleA.label
+		    }, { 
+			"id": idB,
+			"owner": "contact",
+			"access": ["admin","fill"],
+			"fields": 1,
+			"label": exampleB.label
+		    } ]
+		};
+		
+		var list = Test.query("GET",["db/",db,"/forms"]).result();
+
+		Assert.areEqual(expected, list).then(next);
+
+	    });
+	});
+    });
+    
+});
+
+// # Errors
+// 
+// ## Returns `404 Not Found`
+// - ... if database `{db}` does not exist
+
+TEST("Returns 404 when database does not exist.", function(next) {
+    Test.query("GET",["db/00000000000/forms"]).error(404).then(next);
+});
+
+
+// ## Returns `401 Unauthorized` 
+// - ... if the provided token does not grant mathch contact `{as}`.
+
+TEST("Returns 401 when token is not valid.", function(next) {
+    Test.query("GET",["db/00000000000/forms"],{token:"012345789a",id:"0123456789a"})
+	.error(401).then(next);
+});
+ 
+// # Access restrictions
+//
+// Anyone can ask for a list of forms, and will only be shown the forms
+// they can see (**fill** access is required). 
---
+++ test/form/short.js
@@ -0,0 +1,81 @@
+// JSON <short-form>
+// Forms / A short representation of a form
+// 
+// Returned by most API methods that involve multiple forms. It is intended to 
+// provide data relevant for displaying the form within a list.
+// 
+// Typical examples [listing all forms](/docs/#/forms/list.js).
+//
+// ### `ShortContact` JSON format
+//     { id : <id>, 
+//       label : <label> | null, 
+//       fields : <int>,
+//       owner : <owner>,
+//       access : [ <access>, ... ] }
+// 
+// - `id` is the [unique identifier](/docs/#/types/id.js) for this 
+//   form.
+// - `label` is a [human-readable name](/docs/#/types/label.js). Not all forms
+//   have one, because it is not a mandatory property when creating a forms.
+// - `fields` is the number of fields in the form.
+// - `owner` is the owner of filled form instances, currently only `"contact"` 
+//   is supported.
+// - `access` lists the [access levels](/docs/#/form/audience.js) of the viewer
+//   over this form.  
+
+TEST("The form's data is returned.", function(next) {
+
+    var example = {
+	"owner": "contact",	
+	"audience": { "fill": "anyone" },
+	"label": "Personal information",
+	"custom": [1,2,3],
+	"fields": [ {
+	    "id": "1",
+	    "label": "Why did you join our group ?",
+	    "kind": "text",
+	    "required": true
+	}, {
+	    "id": "2",
+	    "label": "How often do you log in ?",
+	    "kind": "single",
+            "choices" : [
+		"Every day",
+		"Once a week",
+		"Once a month",
+		"Less than once a month" ],
+	    "required": false
+	} ]
+    };
+
+    var db = Query.mkdb();
+    var auth = Query.auth(db);
+
+    Test.query("POST",["db/",db,"/forms/create"],example,auth).result("id").then(function(id) {
+
+	var form = Test.query("GET",["db/",db,"/forms"],auth).result("list",0);
+	
+	var expected = {
+	    "id": id,
+	    "label": example.label,
+	    "owner": example.owner,
+	    "fields": 2,
+	    "access": ["admin","fill"]
+	};
+	
+	Assert.areEqual(expected,form).then(next);
+
+    });
+
+});
+
+//
+// ### Example value
+//     { "id" : "0Et9j0026rO",
+//       "label" : "Personal information",
+//       "owner" : "contact",
+//       "fields" : 4,
+//       "access" : ["fill"] }
+//
+// To get more information about a specific form, use the [get single form](/docs/#/form/get.js)
+// endpoint.
