75733ddeb997ac39add435136d434bb3fcae284b
Victor Nicollet <victor@nicollet.net>
2014-04-18 21:38:08 +0200

    Implement SentMailLib.Link

--- server/sentMailLib/link.ml
+++ server/sentMailLib/link.ml
@@ -1,5 +1,54 @@
 (* © 2014 RunOrg *)
 
+open Std
+
+let enc3 n = 
+  let str = String.create 3 in 
+  let n   = str.[2] <- Char.base62_encode (n mod 62) ; n / 62 in
+  let n   = str.[1] <- Char.base62_encode (n mod 62) ; n / 62 in
+  let ()  = str.[0] <- Char.base62_encode (n mod 62) in
+  str
+
+let enc2 n = 
+  let str = String.create 3 in 
+  let n   = str.[2] <- Char.base62_encode (n mod 62) ; n / 62 in
+  let n   = str.[1] <- Char.base62_encode (n mod 62) ; n / 62 in
+  let ()  = str.[0] <- Char.base62_encode (n mod 62) in
+  str
+    
+module Root = struct
+
+  include type module (string)
+
+  let length = 11 + 11 + 3
+
+  let make wid pos = 
+    (I.to_string wid) ^ Token.I.(to_string (gen ())) ^ (enc3 pos) 
+
+end 
+
 include type module (string)
 
 let to_string link = link
+
+let url id link = 
+  Json.String 
+    (!! "%s/db/%s/link/%s"
+	Configuration.Mail.url 
+	(Id.to_string id)
+	(to_string link) )
+
+let track root = 
+  root ^ enc2 0
+
+let self root = 
+  root ^ enc2 1
+
+let view root ln = 
+  root ^ enc2 (2 * ln + 2) 
+
+let auth root ln = 
+  root ^ enc2 (2 * ln + 3) 
+
+let root l = 
+  String.sub l 0 Root.length
--- server/sentMailLib/link.mli
+++ server/sentMailLib/link.mli
@@ -1,5 +1,45 @@
 (* © 2014 RunOrg *)
 
+(** Links and link roots. A typical link identifier is 27 
+    characters long. With a 10-character domain, the full 
+    size of a link URL is 66 characters. Not excellent, but
+    at least it fits on one line in text mode. 
+
+    {[
+https://runorg.com/db/00000000000/link/111111111112222222222233344
+    ]}
+*)
+    
+(** Link roots are common prefixes of the link token that
+    can be used to identify (uniquely!) an (mid,cid) pair.
+    They are also random enough to be tampering-proof, by
+    incorporating a [Token] inside. *)
+module Root : sig
+  include Fmt.FMT
+  val make : I.t -> int -> t
+end 
+
 include Fmt.FMT
 
 val to_string : t -> string
+
+(** Create an URL pointing at a link in the current database. 
+    [{config.mail.url}/db/{db}/link/{id}] *) 
+val url : Id.t -> t -> Json.t
+
+(** Create a link that redirects to the root mail's "self" 
+    url (assuming one was configured). *)
+val self  : Root.t -> t
+
+(** Create a link that tracks the root mail's opening. *)
+val track : Root.t -> t
+
+(** Create a link that authenticates the mail's 'to' contact
+    and redirects to the nth url in the mail. *)
+val auth : Root.t -> int -> t
+
+(** Create a link that redirects to the nth url in the mail. *)
+val view : Root.t -> int -> t
+
+(** Obtain the root of this link. *)
+val root : t -> Root.t
