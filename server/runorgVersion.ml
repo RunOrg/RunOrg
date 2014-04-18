c8d293d50f4a7b603eb6f2e716124aa4ed1c12ce
Victor Nicollet <victor@nicollet.net>
2014-04-18 18:28:05 +0200

    Use Cqrs.StatusView in SentMailLib.View.status

--- server/runorgVersion.ml
+++ server/runorgVersion.ml
@@ -2,7 +2,7 @@
 
 open Std
 
-let build = 48
+let build = 49
 
 let major = 0
 let minor = 1
--- server/sentMailLib/view.ml
+++ server/sentMailLib/view.ml
@@ -7,12 +7,12 @@ let projection = Cqrs.Projection.make "sentmail" O.config
 (* Individual e-mail status by (mid,cid) pair.
    =========================================== *)
 
-module Status = type module [ `Scheduled | `Sent | `Failed ] 
+module Status = type module [ `Unknown | `Scheduled | `Sent | `Failed ] 
 module MidCid = type module (Mail.I.t * CId.t) 
 
 let status = 
 
-  let statusV, status = Cqrs.MapView.make projection "status" 0 
+  let statusV, status = Cqrs.StatusView.make projection "status" 1 `Unknown
     (module MidCid : Fmt.FMT with type t = MidCid.t)
     (module Status : Fmt.FMT with type t = Status.t) in
 
@@ -23,22 +23,28 @@ let status =
 
       let mid = ev # mid in 
       List.M.iter (fun cid -> 
-	Cqrs.MapView.update status (mid,cid) (function 
-	| Some _ -> `Keep
-	| None   -> `Put `Scheduled)) (ev # list) 
+	Cqrs.StatusView.update status (mid,cid) (function 
+	| `Unknown 
+	| `Scheduled -> `Scheduled
+	| `Sent      -> `Sent
+	| `Failed    -> `Failed)) (ev # list) 
 
     | `Sent ev -> 
       
-      Cqrs.MapView.update status (ev # mid, ev # cid) (function 
-      | Some (`Sent|`Failed) -> `Keep 
-      | _ -> `Put `Sent) 
+      Cqrs.StatusView.update status (ev # mid, ev # cid) (function 
+      | `Unknown 
+      | `Scheduled 
+      | `Sent      -> `Sent
+      | `Failed    -> `Failed)
 
     | `LinkFollowed   _ -> return () 
     | `SendingFailed ev -> 
   
-      Cqrs.MapView.update status (ev # mid, ev # cid) (function
-      | Some (`Sent|`Failed) -> `Keep
-      | _ -> `Put `Failed)
+      Cqrs.StatusView.update status (ev # mid, ev # cid) (function
+      | `Unknown 
+      | `Scheduled 
+      | `Failed    -> `Failed
+      | `Sent      -> `Sent)
     
   end in
 
