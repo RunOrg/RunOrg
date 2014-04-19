f59313f96501cf5094400e0aea5b6105aeb9cc1c
Victor Nicollet <victor@nicollet.net>
2014-04-19 11:27:24 +0200

    Split tests and fixtures

---
+++ sites/docs/fixture.js
@@ -0,0 +1,230 @@
+// A fixture is a group of tests for a specific API feature, with 
+// the corresponding documentation. Some fixtures have sub-fixtures.
+
+var Fixture = (function(){
+
+    // Parsing functions used for the fixture contents
+
+    function parseTestFixture(script) {
+	
+	var parsed = { tests: [], status: null, version: null };
+	
+	// The test functions themselves
+	var run = eval("(function(TEST){\n" + script + "\n})")(function(name,fun){
+	    parsed.tests.push({name : name, func: func});
+	});
+	
+	// The lines that are commented (the test fixture test)
+	var lines = script.split("\n");
+	var text  = [];
+	for (var i = 0; i < lines.length; ++i) 
+	    if (/^\s*\/\//.exec(lines[i])) 
+		text.push(lines[i].replace(/^\s*\/\/\s?/,''));	    
+	
+	parsed.query = text.shift();
+	parsed.description = text.shift().replace(/^.*\/\s*/,'');   
+	
+	while (text[0].trim() == '') text.shift();
+	
+	if (/@/.exec(text[0])) {
+	    var current = text.shift().split('@');
+	    parsed.status = current[0].trim();
+	    parsed.version = current[1].trim();
+	}
+	
+	parsed.body = markdown.toHTML(text.join('\n').trim());
+	
+	return parsed;
+    }
+    
+    function parseDoc(md) {
+	
+	var parsed = { tests: [], status: null, version: null, query: null };
+	var text = md.split("\n");
+	
+	parsed.description = text.shift();   
+	
+	while (text[0].trim() == '') text.shift();
+	
+	parsed.body = markdown.toHTML(text.join('\n').trim());
+	
+	return parsed;
+    }
+
+    // Fixture class
+
+    function F(data) {
+	
+	// The HTTP verb (for queries), 'JSON' (for types) or null.
+	this.verb = data.verb || null;
+
+	// The path (for queries), name (for types) or null. 
+	this.path = data.path || null;
+
+	// The source file that contains this fixture on the server.
+	// Some fixtures are generated client-side and do not have a file.
+	this.file = data.file || null;
+
+	// The list of categories leading to this fixture. 
+	this.categories = data.categories;
+
+	// The description (used as title) 
+	this.description = data.description;
+
+	// The number of tests in the fixture. 
+	this.nTests = data.tests || 0;
+
+	// The actual list of tests, will be loaded when necessary. 
+	this._tests = null;
+
+	// The child fixtures
+	this._fixtures = [];
+    }
+
+    F.prototype = {
+
+	// The 'category path' of a fixture, of the form A::B::C
+	catPath: function() { 
+	    var parent = this.catParent();
+	    if (parent) return parent + '::' + this.description; 
+	    return this.description;
+	},
+
+	// The 'category path' of a fixture's parent category fixture, of the form A::B
+	catParent: function() { return this.categories.join('::'); }
+
+	// Add a child fixture to this fixture
+	addChild: function(fixture) {
+	    this._fixtures.push(fixture);
+	},
+
+	// Returns a promise that waits for all child tests (both in this fixture
+	// and in child fixtures) to return. The provided callback function will 
+	// be called whenever a test finishes.
+	run: function(onTestEnd) {	  
+
+	    var self = this;
+
+	    // Always relod tests from scratch. This ensures server-side changes are
+	    // taken into account, AND resets the statistics by clearing the old runs.
+	    this.tests = null;
+	    var myTests = this.loadTests().then(function(tests) {
+
+		// Set to the real array to avoid async access when computing
+		// statistics. 
+		self._tests = tests;
+
+		// Start all tests and wait for them to finish
+		return $.wait.apply($, tests.map(function(test) { return test.run(onTestEnd); }));
+
+	    });
+
+	    var myChildren = $.wait.apply($, this._fixtures.map(function(f) { return f.run(onTestEnd); }));
+
+	    return $.wait(myTests, myChildren);
+	},
+	
+	// Returns an object with statistics about this fixture and
+	// all its child fixtures.
+	//  - tests: the number of tests
+	//  - failed: the number of tests that ran and failed
+	//  - ran: the number of tests that ran
+	stats: function() {
+
+	    var mine = { tests: this.nTests, failed: 0, ran: 0 };
+
+	    if (this._tests !== null) {
+		this._tests.forEach(function(test) {
+		    if (test.ran())    mine.ran++;
+		    if (test.failed()) mine.failed++;
+		});
+	    }
+
+	    this._fixtures.forEach(function(fixture) {
+		var stats = fixture.stats();
+		mine.tests  += stats.tests;
+		mine.failed += stats.failed;
+		mine.ran    += stats.ran;
+	    });
+
+	    return mine;
+	},
+
+	// Returns all tests that have failed. 
+	failed: function() {
+	    if (this._tests === null) return [];
+	    return this._tests.filter(function(test) { return test.failed(); });
+	},
+
+	// Returns a promise that loads the raw contents of this fixture
+	// (if it has a 'file'). 
+	raw: function() {
+	    if (this.file === null) return $.Deferred().resolve('').promise();
+	    return $.get('/docs/' + this.file, function(){}, 'text');
+	},
+
+	// As 'raw()', but parses the fixture.
+	parsed: function() {
+	    var isJs = /.js$/.exec(this.file || '');
+	    return this.raw().then(function(text){
+		return isJs ? parseTextFixture(text) : parseDoc(text);
+	    });
+	},
+
+	// Parses the raw contents of this fixture (if any), and extracts the tests
+	// from it. 
+	loadTests: function() {
+	    return this.parsed().then(function(parsed){
+		return parsed.tests.map(function(t) { return new Test(t.name,t.func); });
+	    });
+	},
+	
+    };
+
+    // A promise that returns all fixtures keyed by path.
+    F.all = $.getJSON("/docs/all.json");
+
+    // A promise that returns the root fixture
+    F.root = F.all.then(function(json) {
+
+	var fixtures = [];
+	var fixturesByCatPath = {};
+	
+	// Fill with found fixtures
+	for (var file in contents) {
+	    var fixture = new F(contents[file]);
+	    fixtures.push(fixture);
+	    fixturesByCatPath[fixture.catPath()] = fixture;
+	}
+	
+	// Fill in any missing parent catPaths 
+	fixtures.forEach(function(fixture) {
+	    
+	    var parent = fixture.catParent();	
+	    if (parent in fixturesByCatPath) continue;
+	    
+	    var categories  = fixture.categories.slice(0);
+	    var description = categories.length == 0 ? "" : categories.pop();
+	    
+	    var parentFixture = new F({
+		categories: categories,
+		description: description
+	    });
+	    
+	    fixtures.push(parentFixture);
+	    fixturesByCatPath[parent] = parentFixture;
+	    
+	});
+	
+	// Connect each fixture to its parent
+	fixtures.forEach(function(fixture) {
+	    fixturesByCatPath[fixture.catParent()].addChild(fixture);
+	});
+	
+	return fixturesByCatPath['']; 
+	
+    });
+
+    return F;
+
+})();
---
+++ sites/docs/test.js
@@ -0,0 +1,57 @@
+var Test = (function() {
+
+    function T(name,func) {
+
+	// The name/description of this test
+	this.name = name;
+
+	// The function used to run this test. Takes a query connection and assert 
+	// object as argument, returns a promise. 
+	this.func = func;
+	
+	// Has this test run ? 
+	this._ran = false;
+
+	// Has this test failed ?
+	this._failed = false;
+
+	// What is the reason for this test's failure ? 
+	this._failure = null;
+    }
+
+    T.prototype = {
+
+	// Run the test. Call the provided callback when it finishes, regardless
+	// of result. 
+	run: function(onTestEnd) {
+
+	    var self = this;
+
+	    this._ran     = false;
+	    this._failed  = false;
+	    this._failure = null;
+
+	    return this.func.call(null,Assert.create(),Query.create())
+		.then(function() { onTestEnd(); self._ran = true; },
+		      function(reason) { 
+			  onTestEnd();
+			  self._ran = true;
+			  self._failed = true;
+			  self._failure = reason;
+		      });
+	},
+
+	// Has this test run ? 
+	ran: function() { return this._ran; },
+
+	// Has this test failed ? 
+	failed: function() { return this._failed; },
+
+	// Why did this test fail ? 
+	failure: function() { return this._failure; }
+
+    };
+
+    return T;
+
+})();
