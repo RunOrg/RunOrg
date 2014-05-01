844d99c503833ca91e9fe856fd71f41b88e5c230
Victor Nicollet <victor@nicollet.net>
2014-05-01 18:05:56 +0200

    Tests for POST /groups/{id}/remove

--- test/group/remove.js
+++ test/group/remove.js
@@ -1,5 +1,5 @@
 // POST /db/{db}/groups/{id}/remove
-// Groups / Remove contacts from a group
+// Groups / Remove members from a group
 // 
 // Alpha @ 0.1.23
 //
@@ -7,32 +7,51 @@
 // [Delayed](/docs/#/concept/delayed.md),
 // [Idempotent](/docs/#/concept/idempotent.md).
 //
-// Removes the provided contacts from the specified group. Contacts not 
-// in the group are silently ignored. 
+// Removes the provided members from the specified group. People not 
+// in the group (or who do not exist) are silently ignored. 
 // 
 // ### Request format
 //     [ <id>, ... ]
 //
-// The contacts are passed as an array of identifiers. 
+// The removed members are passed as an array of identifiers. 
 
-TODO("The response has valid return code and content type.", function(next) {
-    Assert.fail();
+TEST("The response has valid return code and content type.", function(Query) {
+    var db = Query.mkdb();
+    var auth = Query.auth(db);
+    var peon = Query.auth(db,false,"peon@runorg.com");
+    return Query.post(["db/",db,"/groups/admin/remove"],[peon.id],auth)
+	.assertStatus(202).assertIsJson();    
 });
 
-TODO("An empty list is acceptable.", function(next) {
-    Assert.fail();
+TEST("An empty list is acceptable.", function(Query) {
+    var db = Query.mkdb();
+    var auth = Query.auth(db);
+    var peon = Query.auth(db,false,"peon@runorg.com");
+    return Query.post(["db/",db,"/groups/admin/remove"],[],auth)
+	.assertStatus(202);    
 });
 
-TODO("Duplicate contacts in list are acceptable.", function(next) {
-    Assert.fail();
+TEST("Duplicate contacts in list are acceptable.", function(Query) {
+    var db = Query.mkdb();
+    var auth = Query.auth(db);
+    var peon = Query.auth(db,false,"peon@runorg.com");
+    return Query.post(["db/",db,"/groups/admin/remove"],[peon.id,peon.id],auth)
+	.assertStatus(202);    
 });
 
-TODO("Contacts not in group are ignored.", function(next) {
-    Assert.fail();
-});
-
-TODO("Removed contacts can not be found in the group.", function(next) {
-    Assert.fail();
+TEST("Removed contacts can not be found in the group.", function(Query) {
+    var db = Query.mkdb();
+    var auth = Query.auth(db);
+    var peon = Query.auth(db,true,"peon@runorg.com");
+    return Query.post(["db/",db,"/groups/admin/remove"],[peon.id],auth).then(function() {
+	return Query.get(["db/",db,"/groups/admin"],auth).then(function(d,s,r) { 
+	    var list = d.list;
+	    return $.when(
+		Assert.areEqual(1,list.length),
+		Assert.areEqual(auth.id, list[0].id)
+	    );
+	});
+    });
 });
 
 // # Examples
@@ -51,35 +70,53 @@ TODO("Removed contacts can not be found in the group.", function(next) {
 //
 // # Errors
 // 
+// ## Returns `401 Unauthorized` 
+// - ... if the provided token does allow acting as `{as}`.
+
+TEST("Returns 401 when token is not valid.", function(Query) {
+    var db = Query.mkdb();
+    return Query.post(["db/",db,"/groups/admin/remove"],[],{id:"01234567890",token:"01234567890"})
+	.assertStatus(401);
+});
+
+// ## Returns `403 Forbidden`
+// - ... if `{as}` does not have at least **moderate** access to 
+//   group `{id}`.
+
+TEST("Returns 403 when no 'moderate' access.", function(Query) {
+    var db = Query.mkdb();
+    var auth = Query.auth(db); 
+    var id = Query.post(["db/",db,"/groups"],{"audience":{"list":"anyone"}},auth).id();
+    var peon = Query.auth(db,false,"peon@runorg.com");
+    return Query.post(["db/",db,"/groups/",id,"/remove"],[],peon)
+	.assertStatus(403);
+});
+
 // ## Returns `404 Not Found`
 // - ... if database `{db}` does not exist
 
-TODO("Returns 404 when database does not exist.", function(next) {
-    Assert.fail();
+TEST("Returns 404 when database does not exist.", function(Query) {
+    return Query.post(["db/00000000000/groups/admin/remove"],[]).assertStatus(404);
 });
 
 // - ... if group `{id}` does not exist in database `{db}`
 
-TODO("Returns 404 when group does not exist.", function(next) {
-    Assert.fail();
+TEST("Returns 404 when group does not exist.", function(Query) {
+    var db = Query.mkdb();
+    var auth = Query.auth(db); 
+    return Query.post(["db/",db,"/groups/00000000000/remove"],[]).assertStatus(404);
 });
 
-// - ... if one of the removed contacts does not exist in database `{db}`
+// - ... if `{as}` does not have at least **view** access to group `{id}`,
+//   to ensure [absence equivalence](/docs/#/concept/absence-equivalence.md).
 
-TODO("Returns 404 when contact does not exist.", function(next) {
-    Assert.fail();
+TEST("Returns 404 when no 'view' access.", function(Query) {
+    var db = Query.mkdb();
+    var peon = Query.auth(db,false,"peon@runorg.com");
+    return Query.post(["db/",db,"/groups/admin/remove"],[],peon).assertStatus(404);
 });
 
-// ## Returns `401 Unauthorized` 
-// - ... if the provided token does not grant removing from the group,
-//   or no token was provided
-
-TODO("Returns 401 when token is not valid.", function(next) {
-    Assert.fail();
-});
- 
 // # Access restrictions
 //
-// Currently, anyone can remove contacts from a group with a token for the corresponding database. 
-// This is subject to change in future versions.
-
+// An [access level](/docs/#/group/audience.js) of at least **moderate** is required
+// to remove members from a group.
