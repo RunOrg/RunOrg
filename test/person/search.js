df8db63049aa1582afc554f302588febb348822a
Victor Nicollet <victor@nicollet.net>
2014-05-02 09:36:05 +0200

    Restore old tests for GET /people/search?q

--- test/person/search.js
+++ test/person/search.js
@@ -7,24 +7,18 @@
 // [Read-only](/docs/#/concept/read-only.md).
 //
 // Returns a set of people from the database that match the search query. 
-// Follows the rules for [search relevance](/docs/#/concept/search.md).
 // 
 // ### Response format
 //     { "list": [ <short>, ... ] }
 // - `list` is a list of people matching the query, in order of 
 //   decreasing relevance. 
 
-TODO("The response has valid return code and content type.", function(next) {
+TEST("The response has valid return code and content type.", function(Query) {
 
-    var db = Query.mkdb(),
-        token = Query.auth(db),
-    response = Test.query("GET",["db/",db,"/people"],{q:""},token).response();
-
-    response.map(function(r) {
-	Assert.areEqual(200, r.status).then();
-	Assert.isTrue(r.responseJSON, "Response type is JSON").then();
-	Assert.areEqual([],r.responseJSON).then();
-    }).then(next);
+    var db = Query.mkdb();
+    var auth = Query.auth(db);
+    return Query.get(["db/",db,"/people?q="],auth)
+	.assertStatus(200).assertIsJson();
 
 });
 
@@ -46,50 +40,60 @@ TODO("The response has valid return code and content type.", function(next) {
 //         "gender" : "M",
 //         "pic" : "https://www.gravatar.com/avatar/5a31b00f649489a9a24d3dc3e8b28060" } ] }
 
-TODO("Returns data for matching people.", function(next) {
+TEST("Returns data for matching people.", function(Query) {
 
     var example = [ { "email": "test@runorg.com" },
 		    { "email" : "vnicollet@runorg.com",
-		      "fullname" : "Victor Nicollet",
+		      "name" : "Victor Nicollet",
 		      "gender" : "M" } ];
 
-    var db = Query.mkdb(), token = Query.auth(db);
-    Test.query("POST",["db/",db,"/people/import"],example,token).result('created').map(function(ids){ 
-	    
-	var list = Test.query("GET",["db/",db,"/people/search"],{q:"vic"},token).result("list");
+    var db   = Query.mkdb();
+    var auth = Query.auth(db);
+    return Query.post(["db/",db,"/people/import"],example,auth).then(function(d,s,r) {
+
+	var ids = d.imported;	    
+	var list = Query.get(["db/",db,"/people/search?q=vic"],auth)
+	    .then(function(d) { return d.list; });
 
 	var expected = [ 
 	    { "id" : ids[1], 
-	      "name" : "Victor Nicollet",
+	      "label" : "Victor Nicollet",
 	      "gender" : "M", 
-	      "pic" : "https://www.gravatar.com/avatar/5a31b00f649489a9a24d3dc3e8b28060" } ];
+	      "pic" : "https://www.gravatar.com/avatar/5a31b00f649489a9a24d3dc3e8b28060?d=identicon" } ];
 	
-	Assert.areEqual(expected, list).then(next);
+	return Assert.areEqual(expected, list);
 
-    }).then();
+    });
 });
 
 //
 // # Errors
 //
+// ## Returns `401 Unauthorized` 
+// - ... if the provided token does not allow acting as `{as}`
+
+TEST("Returns 401 when token is not valid.", function(Query) {
+    var db = Query.mkdb();
+    return Query.get(["db/",db,"/people/search?q="],{id:"01234567890",token:"01234567890"})
+	.assertStatus(401);
+});
+ 
+// ## Returns `403 Forbidden` 
+// - ... if `{as}` is not a database administrator
+
+TEST("Returns 403 when not an admin.", function(Query) {
+    var db = Query.mkdb();
+    var peon = Query.auth(db, false);
+    return Query.get(["db/",db,"/people/search?q="],peon).assertStatus(403);
+});
+
 // ## Returns `404 Not Found`
 // - ... if database `{db}` does not exist
 
-TODO("Returns 404 when database does not exist.", function(next) {
-    Test.query("GET","/db/00000000001/people/00000000002/").error(404).then(next);
+TEST("Returns 404 when database does not exist.", function(Query) {
+    return Query.get("/db/00000000001/people/00000000002/search?q=").assertStatus(404);
 });
 
-// ## Returns `401 Unauthorized` 
-// - ... if the provided token does not grant access to all people,
-//   or no token was provided
-
-TODO("Returns 401 when token is not valid.", function(next) {
-    var db = Query.mkdb();
-    Test.query("GET",["db/",db,"/people"]).error(401).then(function() {
-	Test.query("GET",["db/",db,"/people"],{},"0123456789a").error(401).then(next);
-    });
-});
- 
 // # Access restrictions
 //
 // Currently, anyone can list all people with a token for the corresponding database. 
