7861ae98daff5166df31956ffdfc19bf28fe7495
Victor Nicollet <victor@nicollet.net>
2014-06-19 11:02:00 +0200

    /docs/people/all.md

---
+++ docs/people/all.md
@@ -0,0 +1,73 @@
+<page title="List all people in the database"
+      api="GET /db/{db}/people"
+      js="static RunOrg.Person.List" 
+      tags="methods:person" />
+
+<doc for="js">
+  `RunOrg.Person.List(limit, offset)` reads `limit` people from the 
+  database, starting at `offset`. The order is unspecified but 
+  consistent. 
+
+  Returns a [promise](/concepts/promise.md) that will resolve to a 
+  list of filled `RunOrg.Person` instances. 
+</doc>
+<example type="js" caption="Example usage">
+  RunOrg.Person.List(10, 0).then(function(list) {
+    for (var i = 0; i < list.length; ++i) 
+      console.log("Person %s is %s", list[i].id, list[i].label)    
+  })			 
+</example>
+
+<todo> Explain how to handle errors. </todo>
+<todo> Implement and document extracting "count" from the API in JS </todo>
+
+<doc for="api">
+  Returns all the people in the database, in an unspecified but consistent
+  order, with [pagination](/concepts/pagination.md). 
+</doc>
+
+<fields caption="response fields" for="api">
+  | list  | <person> array | A list of people on the requested page.
+  | count | integer        | The total number of people in the database.                          
+</fields>
+
+<example type="api" caption="Example request">
+  GET /db/0SNQc00211H/people?limit=3&offset=213
+</example>
+<example type="api" caption="Example response">
+  200 OK
+  Content-Type: application/json 
+    
+  { "list" : [ 
+    { "id" : "0SNQe00311H",
+      "name" : "test@runorg.com",
+      "gender" : null,
+      "pic" : "https://www.gravatar.com/avatar/1ed54d253636f5b33eff32c2d5573f70" },
+    { "id" : "0SNQg00511H",
+      "name" : "vnicollet@runorg.com",
+      "gender" : null,
+      "pic" : "https://www.gravatar.com/avatar/5a31b00f649489a9a24d3dc3e8b28060"} ],
+    "count" : 215 }
+</example>
+
+<doc>
+
+# Errors
+
+## Returns `401 Unauthorized` 
+- if the provided token and requester do not match. 
+
+## Returns `403 Forbidden` 
+- if the requester is not a database administrator
+
+## Returns `404 Not Found`
+- if the database does not exist
+
+# Access restrictions
+
+Only [database administrators](/groups/admin.md) may list all people
+in the database. 
+
+Note that [viewing individuals](/people/get.js) is open to anyone.
+
+</doc>
--- test/person/all.js
+++ test/person/all.js
@@ -1,21 +1,5 @@
 // GET /db/{db}/people
 // People / List all people in the database
-//
-// Beta @ 0.9.0
-// 
-// `200 OK`, 
-// [Read-only](/docs/#/concept/read-only.md),
-// [Paginated](/docs/#/concept/paginated.md).
-//
-// Returns all the people in the database, in arbitrary order.
-// Supports `limit` and `offset` pagination. 
-//
-// ### Response format
-//     { "list": [ <person>, ... ],
-//       "count": <int> }
-// - `list` is a list of people on the requested page, in 
-//   [short format](/docs/#/person/short.js)
-// - `count` is the total number of people in the database.
 
 TEST("The response has valid return code and content type.", function(Query) {
     var db = Query.mkdb();
@@ -23,26 +7,6 @@ TEST("The response has valid return code and content type.", function(Query) {
     return Query.get(["db/",db,"/people"],auth)
 	.assertStatus(200).assertIsJson();
 });
-
-// # Examples
-// 
-// ### Example request
-//     GET /db/0SNQc00211H/people?limit=3&offset=213
-// 
-// ### Example response
-//     200 OK
-//     Content-Type: application/json 
-//     
-//     { "list" : [ 
-//       { "id" : "0SNQe00311H",
-//         "name" : "test@runorg.com",
-//         "gender" : null,
-//         "pic" : "https://www.gravatar.com/avatar/1ed54d253636f5b33eff32c2d5573f70" },
-//       { "id" : "0SNQg00511H",
-//         "name" : "vnicollet@runorg.com",
-//         "gender" : null,
-//         "pic" : "https://www.gravatar.com/avatar/5a31b00f649489a9a24d3dc3e8b28060"} ],
-//       "count" : 215 }
 
 TEST("Returns correct number of people in count.", function(Query) {
 
@@ -95,19 +59,11 @@ TEST("Returns data for all people.", function(Query) {
 
 });
 
-// # Errors
-//
-// ## Returns `401 Unauthorized` 
-// - ... if the provided token does allow acting as `{as}`.
-
 TEST("Returns 401 when token is not valid.", function(Query) {
     var db = Query.mkdb();
     return Query.get(["db/",db,"/people"],{id:"01234567890",token:"01234567890"})
 	.assertStatus(401);
 });
-
-// ## Returns `403 Forbidden` 
-// - ... if `{as}` is not a database administrator
 
 TEST("Returns 403 when not an admin.", function(Query) {
     var db = Query.mkdb();
@@ -115,16 +71,6 @@ TEST("Returns 403 when not an admin.", function(Query) {
     return Query.get(["db/",db,"/people"],peon).assertStatus(403);
 });
   
-// ## Returns `404 Not Found`
-// - ... if database `{db}` does not exist
-
 TEST("Returns 404 when database does not exist.", function(Query) {
     return Query.get("/db/00000000001/people/00000000002/").assertStatus(404);
 });
-
-// # Access restrictions
-//
-// Only [database administrators](/docs/#/group/admin.md) may list all people
-// in the database. 
-//
-// Note that [viewing individuals](/docs/#/contact/get.js) is open to anyone.
