27ed4c39a41a4ff4d5281fdbf6494098d8263996
Victor Nicollet <victor@nicollet.net>
2014-06-30 19:14:23 +0200

    Start work on form docs

--- test/form/create.js
+++ test/form/create.js
@@ -1,35 +1,3 @@
-// POST /db/{db}/forms
-// Forms / Create a form
-//
-// Beta @ 0.9.0
-//
-// `202 Accepted`, [Delayed](/docs/#/concept/delayed.md), 
-//  sometimes [Idempotent](/docs/#/concept/idempotent.md).
-//
-// Person [`{as}`](/docs/#/concept/as.md) creates a new form, initially unfilled.
-//
-// ### Request format
-//     { "id" : <customid> | null,
-//       "label" : <label> | null,
-//       "custom": <json>,
-//       "owner": "person",
-//       "fields": [ <field>, ... ],
-//       "audience": <audience> }
-// - `id` is an optional [custom identifier](/docs/#/types/custom-id.js).
-// - `label` is an optional [human-readable name](/docs/#/types/label.js).
-// - `custom` is an arbitrary block of JSON that will be stored as-is by 
-//   RunOrg and returned as such by the API.
-// - `owner` is the nature of the owners associated with each filled instance
-//   of the form. In the current version of the API, this is always `"person"`
-//   (each instance is owned by a person). 
-// - `fields` is an ordered list of [fields](/docs/#/form/field.js) to be 
-//   filled.
-// - `audience` is the [audience](/docs/#/form/audience.js) of the form.
-// 
-// ### Response format
-//     { "id" : <id>,
-//       "at" : <clock> }
-
 var Example = { 
     "owner": "person",
     "audience": {},
@@ -45,15 +13,6 @@ TEST("The response has valid return code and content type.", function(Query) {
     return response.assertStatus(202).assertIsJson();
 
 });
-
-// 
-// The behaviour of the request depends on whether an identifier was provided: 
-// **force-id** mode or **generate-id** mode. 
-//
-// In force-id mode, RunOrg will attempt to create a form with that identifier,
-// and do nothing if such form already exists ([idempotent](/docs/#/concept/idempotent.md)
-// request), even if it is different. In other words, these are _create if not exists_
-// semantics rather than _create or replace_. 
 
 TEST("Form with forced id appears.", function(Query) {
 
@@ -71,11 +30,6 @@ TEST("Form with forced id appears.", function(Query) {
 
 });
 
-//
-// In generate-id mode, a new form identifier is generated so that no collisions may
-// occur, and a brand new form is created. Repeating the request will generate a new
-// form. 
-
 TEST("Multiple creations create multiple forms.", function(Query) {
 
     var db = Query.mkdb();
@@ -88,53 +42,11 @@ TEST("Multiple creations create multiple forms.", function(Query) {
 
 });
 
-//
-// ### Example request
-//     POST /db/0SNQc00211H/forms
-//     Content-Type: application/json
-// 
-//     { "label" : "Personal information", 
-//       "owner" : "person",
-//       "audience" : {
-//         "admin": {},
-//         "fill": { groups: [ "0SNQe0032JZ" ] } },
-//       "fields" : [ {
-//         "id" : "1",
-//         "label" : "Why did you join our group ?",
-//         "kind" : "text",
-//         "required" : true
-//       }, {
-//         "id" : "2", 
-//         "label" : "How often do you log in ?",
-//         "kind" : "single",
-//         "choices" : [
-//           "Every day",
-//           "Once a week",
-//           "Once a month",
-//           "Less than once a month" ],
-//         "required" : false
-//       } ] }
-//
-// ### Example response
-//     202 Accepted
-//     Content-Type: application/json
-// 
-//     { "id" : "0SNQe0032JZ",
-//       "at" : [[2,219]] }
-  
-// # Errors
-// 
-// ## Returns `404 Not Found`
-// - ... if database `{db}` does not exist
-
 TEST("Returns 404 when database does not exist.", function(Query) {
 
     return Query.post(["db/00000000000/forms"],Example).assertStatus(404);
 
 });
-
-// ## Returns `403 Forbidden`
-// - ... if person `{as}` cannot create a form.
 
 TEST("Returns 403 when person cannot create a form.", function(Query) {
 
@@ -144,10 +56,6 @@ TEST("Returns 403 when person cannot create a form.", function(Query) {
     return Query.post(["db/",db,"/forms"],Example,auth).assertStatus(403);
     
 });
-
-// ## Returns `400 Bad Request`
-// - ... if the provided identifier is not a valid [custom 
-//   identifier](/docs/#/types/custom-id.js)
 
 TEST("Returns 400 when custom id is invalid.", function(Query) {   
 
@@ -165,9 +73,6 @@ TEST("Returns 400 when custom id is invalid.", function(Query) {
 
 });
 
-// ## Returns `409 Conflict`
-// - ... if a form already exists with the provided identifier.
-
 TEST("Returns 409 when the form exists.", function(Query) {
 
     var example = $.extend({ "id": "personal" }, Example);
@@ -180,9 +85,6 @@ TEST("Returns 409 when the form exists.", function(Query) {
     });
 });
 
-// ## Returns `401 Unauthorized` 
-// - ... if the provided token does not match person `{as}`.
-
 TEST("Returns 401 when token is not valid.", function(Query) {
 
     var db = Query.mkdb();
@@ -191,6 +93,3 @@ TEST("Returns 401 when token is not valid.", function(Query) {
 
 });
 
-// # Access restrictions
-//
-// Person `{as}` must be a [database administrator](/docs/#/group/admin.md). 
