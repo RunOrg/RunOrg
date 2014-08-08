39c6a3c118aae84f5e549b57e24929cc198881b0
Victor Nicollet <victor@nicollet.net>
2014-08-08 10:52:36 +0200

    Chatroom tests

--- test/chat/get.js
+++ test/chat/get.js
@@ -1,102 +1,58 @@
-// GET /db/{db}/chat/{id}
-// Chatrooms / Read chatroom properties
-//
-// Beta @ 0.9.0
-//
-// `200 OK`, 
-// [Read-only](/docs/#/concept/read-only.md).
-//
-// Reads chatroom information (except the list of messages). 
-// Data is returned using a [client join pattern](/docs/#/concept/client-join.md): 
-//
-// ### Response format
-//     { "contacts": [ <shortcontact>, ... ],
-//       "groups": [ <groupinfo>, ... ],
-//       "info": <chatinfo> }
-//  - `contacts` is a list of [short profiles](/docs/#/contact/short-contact.js) for 
-//    all participants in the chatroom.
-//  - `groups` is the [basic information](/docs/#/group/group-info.js)
-//    for all groups present in the chatroom.
-//  - `info` is a [chatroom information object](/docs/#/chat/chat-info.js).
+// GET /chat/{id}
 
-TODO("The response has valid return code and content type.", function(next) {
-    Assert.fail();
+TEST("The response has valid return code and content type.", function(Query) {
+
+    var db = Query.mkdb();
+    var auth = Query.auth(db);
+    var id = Query.post(["db/",db,"/chat"],{},auth).id();
+    return Query.get(["db/",db,"/chat/",id],auth)
+	.assertStatus(200).assertIsJson();
 });
 
-// # Examples
-// 
-// ### Example request
-//     GET /db/0SNQc00211H/chat/0SNQe0132FW
-// 
-// ### Example response
-//     200 OK
-//     Content-Type: application/json 
-//     
-//     { "contacts": [
-//       { "id" : "0SNQe00311H",
-//         "name" : "test@runorg.com",
-//         "gender" : null,
-//         "pic" : "https://www.gravatar.com/avatar/1ed54d253636f5b33eff32c2d5573f70" },
-//       { "id" : "0SNQg00511H",
-//         "name" : "vnicollet@runorg.com",
-//         "gender" : null,
-//         "pic" : "https://www.gravatar.com/avatar/5a31b00f649489a9a24d3dc3e8b28060"} ],
-//       "groups": [
-//         { "id": "0SNQe0032JZ",
-//         "label": "Team members",
-//         "count" : 215 } ],
-//       "info": { 
-//         "id": 0SNQe0132FW,
-//         "contacts": ["0SNQe00311H","0SNQg00511H"],
-//         "groups": ["0SNQe0032JZ"],
-//         "subject": "Preparation for the next meeting",
-//         "public": false,
-//         "count": 118 }}
+TEST("Returns correct data as admin.", function(Query) {
 
-TODO("Returns correct list of contacts.", function(next) {
-    Assert.fail();
-});
+    var created = {
+	subject: "Hello, world!",
+	custom: { a: true },
+	audience: { admin: "anyone" }
+    };
 
-TODO("Returns correct list of groups.", function(next) {
-    Assert.fail();
-});
+    var db = Query.mkdb();
+    var auth = Query.auth(db);
+    var id = Query.post(["db/",db,"/chat"],created,auth).id();
+    return Query.get(["db/",db,"/chat/",id],auth).then(function(value) {
 
-TODO("Returns correct message count.", function(next) {
-    Assert.fail();
+	var expected = $.extend({
+	    id: id,
+	    count: 0,
+	    access: [ "admin", "view" ]	   
+	}, created);
+	
+	delete value.last;
+	
+	return Assert.areEqual(expected, value);
+
+    });
+
 });
 
 TODO("Returns correct subject.", function(next) {
     Assert.fail();
 });
 
-// 
-// # Errors
-// 
-// ## Returns `404 Not Found`
-// - ... if database `{db}` does not exist
 
 TODO("Returns 404 when database does not exist.", function(next) {
     Assert.fail();
 });
 
-// - ... if chatroom `{id}` does not exist in database `{db}`
 
 TODO("Returns 404 when chatroom does not exist.", function(next) {
     Assert.fail();
 });
 
-// ## Returns `401 Unauthorized` 
-// - ... if the provided token does not grant access to the chatroom,
-//   or no token was provided
-
 TODO("Returns 401 when token is not valid.", function(next) {
     Assert.fail();
 });
  
-// # Access restrictions
-//
-// Currently, anyone can read the basic details of a chatroom with a token for the corresponding database. 
-// This is subject to change in future versions.
-       
 
 
