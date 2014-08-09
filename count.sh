d6e243e089aa15aacb3e80a48c2165877e00b9a8
Victor Nicollet <victor@nicollet.net>
2014-08-09 14:27:31 +0200

    Counting lines

--- count.sh
+++ count.sh
@@ -8,15 +8,17 @@ find plang -name '*.js' >> .alljsfiles
 
 find . -name '*.css'  > .allcssfiles
 
-find test -name '*.md' | xargs cat > .alldoc
-find test -name '*.js' | xargs cat | sed -e '/^\/\//p; /.*/d' >> .alldoc
+find docs -name '*.htm' | xargs cat > .alldoc
+find test -name '*.js' | xargs cat > .alltest
 
 echo "== OCaml: " `cat .allmlfiles | xargs cat | wc -l` "lines"
 echo "== JS   : " `cat .alljsfiles | xargs cat | wc -l` "lines"
 echo "== CSS  : " `cat .allcssfiles | xargs cat | wc -l` "lines"
+echo "== Tests: " `cat .alltest | wc -l` "lines"
 echo "== Docs : " `cat .alldoc | wc -w` "words"
 
 rm .allmlfiles 
 rm .alljsfiles 
 rm .allcssfiles 
 rm .alldoc 
+rm .alltest
